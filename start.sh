a575fd92caa0c7c5b090f6a8311511e3eb3cf50b
aurnik <aurnikislam@gmail.com>
2023-11-01 16:21:43 -0500

    Mount to attached volume

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/bin/surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
+/bin/surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://data/srdb.db
